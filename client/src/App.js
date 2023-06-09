661ee03b4b78ffb0f05d28b0878da7180be3d9e4
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-06-09 14:14:21 +0300

    set client layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './Components/Sidebar';
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app-container'> 
+        <Sidebar/>
+        <div className='App'>
+          <div className='component-container'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ client/src/Components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../App.css"
+import {SidebarData} from './SidebarData'
+
+function Sidebar (){
+
+    const user={
+        avatar:'https://cdn-icons-png.flaticon.com/512/147/147133.png'
+    }
+    return(
+        <div className='Sidebar'>
+            <img
+                className='avatar'
+                src= {user.avatar}>
+            </img>
+            <ul className='SidebarList'>
+            {SidebarData.map((val,key)=>{
+                return(<li key={key} 
+                    className='row'
+                    id={window.location.pathname == val.link ? "active" : ""} 
+                    onClick={()=>{window.location.pathname =  val.link}}
+                    >
+                    <div id='icon'>{val.icon}</div>
+                    <div id='title'>{val.title}</div>
+                </li>)
+            })}
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
