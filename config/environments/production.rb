7aa3f5c7175d9a48043fac9b63a4fe3fe1d787ac
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-06-14 11:21:58 +0300

    add build command

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.require_master_key = true 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
